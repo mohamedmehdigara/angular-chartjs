01a843c3af51ceb810ff840f64c7fa5531f144db
mohamedmehdigara <garamehdi57@yahoo.com>
2023-02-16 14:31:16 +0100

    added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieChartComponent,
+    DoughnutChartComponent
   ],
   imports: [
     BrowserModule,
